c4f632fe41b8d05015d1c9c8df9f409397876e33
Foucault Jérémie <foucaultjeremie@gmail.com>
2014-11-01 15:52:44 +0100

    Méthode delete pour un atelier

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 	var express = require('express'),
-	app = module.exports = express(),
-	server = require('http').createServer(app),
-	ent = require('ent'),
-	mongoose = require('mongoose');
+		app = module.exports = express(),
+		server = require('http').createServer(app),
+		ent = require('ent'),
+		mongoose = require('mongoose');
+	
 	mongoose.connect('mongodb://localhost/it340');
-
 
 	var listeAtelier = mongoose.Schema({
 		id: Number,
@@ -22,16 +22,32 @@
 	var Atelier = mongoose.model('Atelier', listeAtelier);
 
 	//Détail des ateliers
-	app.get('/atelier/:id', function(req, res) {
+	app.get('/atelier/:id', function (req, res) {
 		res.end('Détail ateliers');
 
 		//res.render('atelier.ejs', {id: req.params.id});
 	});
 
 	//Liste des ateliers
-	app.get('/ateliers', function(req, res) {
+	app.get('/ateliers', function (req, res) {
 		res.end("Liste des ateliers");
 		//res.render('ateliers.ejs');
+	});
+
+	// Supression d'un atelier
+	app.delete('/atelier/:id', function (req, res){
+	  	Atelier.findByIdAndRemove(req.params.id, function (err, atelier) {
+			if (err) {
+				console.log("Erreur suppression");
+				res.send(500, { error: err })
+			} else if (atelier) {
+				console.log("Atelier supprimé");
+				res.send(200)
+			} else {
+				console.log("Atelier non trouvé")
+				res.send(404)
+			}
+		});
 	});
 
 	console.log("Server running listening on port 8080 ...");
