68457ead6dc508b1f971e43ba8604d8ed13af5cc
Foucault Jérémie <foucaultjeremie@gmail.com>
2014-11-05 12:02:41 +0100

    Desactivation du cache pour les vues swig

--- app.js
+++ app.js
@@ -17,6 +17,13 @@
 	    app.use(express.static(__dirname + '/public'));
 	});
 
+	// Swig will cache templates for you, but you can disable
+	// that and use Express's caching instead, if you like:
+	app.set('view cache', false);
+	// To disable Swig's cache, do the following:
+	swig.setDefaults({ cache: false });
+	// Don't leave both of these to `false` in production!
+
 
 	var listeAtelier = mongoose.Schema({
 		nom:String,
